c1a6bc8c254eb4a1781389e21934a3ffa12cc12b
Daniel Sánchez <danisanchez86@gmail.com>
2017-01-31 12:18:50 +0100

    Class Book created

---
+++ HackerBooks/Book.swift
@@ -0,0 +1,35 @@
+//
+//  Book.swift
+//  HackerBooks
+//
+//  Created by Daniel Sánchez on 31/1/17.
+//  Copyright © 2017 Daniel Sánchez. All rights reserved.
+//
+
+import Foundation
+
+typealias Authors = Array<String>
+
+class Book {
+    
+    //MARK: - Stored Properties
+    let title   :   String
+    let author  :   Authors
+    let tags    :   Array<String>
+    let imageURL:   URL
+    let url     :   URL
+    
+    //MARK: - Intialization
+    init(title: String,
+         author: Authors,
+         tags: Array<String>,
+         imageURL: URL,
+         url: URL)
+    {
+        self.title = title
+        self.author = author
+        self.tags = tags
+        self.imageURL = imageURL
+        self.url = url
+    }
+}

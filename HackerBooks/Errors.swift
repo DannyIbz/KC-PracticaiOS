36d9e68fdda7fc3be2ded189a60a90d35c56f8d2
Daniel Sánchez <danisanchez86@gmail.com>
2017-01-31 19:51:50 +0100

    Created Error.swift file

---
+++ HackerBooks/Errors.swift
@@ -0,0 +1,16 @@
+//
+//  Errors.swift
+//  HackerBooks
+//
+//  Created by Daniel Sánchez on 31/1/17.
+//  Copyright © 2017 Daniel Sánchez. All rights reserved.
+//
+
+import Foundation
+
+enum BooksError : Error {
+    
+    case wrongURLFormat
+    case resourcePointedByURLNotReachable
+    case jsonParsingError
+}

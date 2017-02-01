f48b3b223f693f0e7856cae83496d89777dcd2f9
Daniel Sánchez <danisanchez86@gmail.com>
2017-02-01 20:36:00 +0100

    JSONProcessing function created

--- HackerBooks/JSONProcessing.swift
+++ HackerBooks/JSONProcessing.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import UIKit
+
+//MARK: - Aliases
+typealias JSONObject        =   AnyObject
+typealias JSONDictionary    =   [String: JSONObject]
+typealias JSONArray         =   [JSONDictionary]
+
+//MARK: - Decodification
+func decode(HackerBooksBook json: JSONDictionary) throws -> Book {
+    
+    guard let urlString = json["pdf_url"] as? String,
+          let url = URL(string: urlString) else {
+          
+            throw BooksError.wrongURLFormat
+    }
+    
+    guard let imageUrl = json["image_url"] as? String,
+          let image = URL(string: imageUrl) else {
+            
+            throw BooksError.resourcePointedByURLNotReachable
+    }
+    
+    let title   =   json["title"] as? String
+    let author  =   json["authors"] as? Authors
+    let tags    =   json["tags"] as? Tags
+    
+    return Book(title: title,
+                author: author,
+                tags: tags,
+                imageURL: image,
+                url: url)
+}
+
+func decode(HackerBooksBook json: JSONDictionary?) throws -> Book {
+    
+    guard let json = json else {
+        throw BooksError.jsonParsingError
+    }
+    
+    return try decode(HackerBooksBook: json)
+}

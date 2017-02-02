58635495e8f340f451f51d027b7695a6b6fb56e1
Daniel Sánchez <danisanchez86@gmail.com>
2017-02-02 23:11:49 +0100

    Tags class added

---
+++ HackerBooks/Tags.swift
@@ -0,0 +1,22 @@
+//
+//  Tags.swift
+//  HackerBooks
+//
+//  Created by Daniel Sánchez on 2/2/17.
+//  Copyright © 2017 Daniel Sánchez. All rights reserved.
+//
+
+import Foundation
+
+typealias tags = Array<String>
+
+class Tags {
+    
+    //MARK: - Properties
+    let name : tags?
+    
+    init(name: tags?){
+        self.name = name
+    }
+    
+}
